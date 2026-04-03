3e548408a7f83dfca22c67705c0bf69e13e71194
Sakshi Shelke <sakshishelke1323@gmail.com>
2026-04-03 20:18:54 +0530

    updated frontend with API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://recommender-backend-b4rq.onrender.com/recommend/1")
+      .then((res) => res.json())
+      .then((result) => setData(result))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Movie Recommendations</h1>
+      <ul>
+        {data.map((item, index) => (
+          <li key={index}>
+            {item.movie} - {item.rating}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
