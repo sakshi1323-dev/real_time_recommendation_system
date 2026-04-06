53ecda464018d0ba88011ae1fe3684ed1aaa5b26
Sakshi Shelke <sakshishelke1323@gmail.com>
2026-04-06 19:10:12 +0530

    Fixed UI issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,35 +4,69 @@ import "./App.css";
 function App() {
   const [userId, setUserId] = useState("");
   const [recommendations, setRecommendations] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const fetchRecommendations = async () => {
-    const res = await fetch(`https://your-backend.onrender.com/recommend/${userId}`);
-    const data = await res.json();
-    setRecommendations(data);
+    console.log("BUTTON CLICKED ");
+
+    if (!userId) {
+      alert("Please enter User ID");
+      return;
+    }
+
+    try {
+      setLoading(true);
+
+      
+      const res = await fetch(`https://recommender-backend-b4rq.onrender.com/recommend/${userId}`);
+
+      if (!res.ok) {
+        throw new Error("API not responding");
+      }
+
+      const data = await res.json();
+      console.log("DATA:", data);
+
+      setRecommendations(data);
+    } catch (error) {
+      console.error("ERROR:", error);
+      alert("Failed to fetch recommendations");
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
     <div className="app">
       
-      {/* Navbar */}
+     
       <div className="navbar">
         <h1>🎬 MovieFlix</h1>
       </div>
 
-      {/* Input Section */}
+     
       <div className="input-section">
         <input
           type="number"
           placeholder="Enter User ID"
           value={userId}
-          onChange={(e) => setUserId(e.target.value)}
+          onChange={(e) => setUserId(Number(e.target.value))}
         />
+
         <button onClick={fetchRecommendations}>
           Get Recommendations
         </button>
       </div>
 
-      {/* Recommendations */}
+     
+      {loading && <p>Loading recommendations...</p>}
+
+      
+      {!loading && recommendations.length === 0 && (
+        <p>No recommendations yet</p>
+      )}
+
+      
       <div className="grid">
         {recommendations.map((rec, index) => (
           <div className="card" key={index}>
